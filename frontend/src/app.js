4cfb50aa6c70f1310a3321649de8ab1370f7c574
Sian Alcock <sian.alcock@lawena.org>
2019-10-11 16:44:44 +0100

    Django plus react to test deployment to AWS

---
+++ frontend/src/app.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  render(){
+    return(
+      <h2>Hello World!</h2>
+
+    )
+  }
+}
+
+
+
+
+
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
